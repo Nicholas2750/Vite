463b2a6992df7372d99fff4f98a4ff1f60314261
Nicholas2750 <nicholashusin@gmail.com>
2020-10-25 19:21:58 -0500

    Add setup for page serving

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, request, render_template
 import mysql
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/static', template_folder='html')
 
 @app.route('/')
 def index():
-  return 'Hello from our DigitalOcean server'
+  return render_template('index.html')
+
+@app.route('/<path:path>')
+def serve_html(path):
+  return render_template(f'{path}.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
