39a1d42e22a7891b1adcf49a4e025c22e0ffc584
Derek Li <derekli2@illinois.edu>
2020-10-29 05:48:10 +0000

    Snake case

--- sqlqueries.py
+++ sqlqueries.py
@@ -7,17 +7,17 @@ get_athlete = "SQL HERE"
 get_data_point = "SQL HERE"
 
 '''Post Requests'''
-addRide = "SQL HERE"
-addRace = "SQL HERE"
-addAthlete = "SQL HERE"
+add_ride = "SQL HERE"
+add_race = "SQL HERE"
+add_athlete = "SQL HERE"
 
 '''Put Requests'''
-modifyRide = "SQL HERE" # only activity name should be able to change
-modifyRace = "SQL HERE" # type, race_name, and date can change
-modifyAthlete = "SQL HERE" # only age, name can change
+modify_ride = "SQL HERE" # only activity name should be able to change
+modify_race = "SQL HERE" # type, race_name, and date can change
+modify_athlete = "SQL HERE" # only age, name can change
 
 '''Delete Requests'''
-deleteRide = "SQL HERE"
-deleteAthlete = "SQL HERE"
+delete_ride = "SQL HERE"
+delete_athlete = "SQL HERE"
 
 
