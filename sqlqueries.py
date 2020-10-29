597bc5bc84d9fa8508943f81d5ccc7d3bfd7d07c
Derek Li <derekli2@illinois.edu>
2020-10-29 05:44:51 +0000

    Added skeloton for where SQL queries will be placed

---
+++ sqlqueries.py
@@ -0,0 +1,23 @@
+# SQL Queries
+
+'''Get Requests'''
+get_ride = "SQL HERE"
+get_race = "SQL HERE"
+get_athlete = "SQL HERE"
+get_data_point = "SQL HERE"
+
+'''Post Requests'''
+addRide = "SQL HERE"
+addRace = "SQL HERE"
+addAthlete = "SQL HERE"
+
+'''Put Requests'''
+modifyRide = "SQL HERE" # only activity name should be able to change
+modifyRace = "SQL HERE" # type, race_name, and date can change
+modifyAthlete = "SQL HERE" # only age, name can change
+
+'''Delete Requests'''
+deleteRide = "SQL HERE"
+deleteAthlete = "SQL HERE"
+
+
