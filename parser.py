80b9f924cedf9f1d1cdd88428e51760b69213e0e
Nicholas2750 <nicholashusin@gmail.com>
2020-11-01 14:21:15 -0600

    Add parser helper for ride GPX

---
+++ parser.py
@@ -0,0 +1,32 @@
+import gpxpy
+
+# Parses the content of a GPX file for a ride and returns a dictionary containing
+# all of its relevant data.
+def parse_ride(file_content):
+  gpx = gpxpy.parse(file_content)
+  data = {}
+
+  data['name'] = gpx.name
+  data['time'] = gpx.time
+  data['datapoints'] = []
+
+  for track in gpx.tracks:
+    for segment in track.segments:
+      for point in segment.points:
+        datapoint = {}
+        datapoint['longitude'] = point.longitude
+        datapoint['latitude'] = point.latitude
+        datapoint['elevation'] = point.elevation
+        datapoint['time'] = point.time
+
+        for extension in point.extensions:
+          if extension.tag in ['heartrate', 'cadence', 'power']:
+            datapoint[extension.tag] = extension.text
+
+          # Strava's temperature data point in the GPX is misformatted.
+          if 'Temperature' in extension.tag:
+            datapoint['temperature'] = extension.text
+
+        data['datapoints'].append(datapoint)
+
+  return data
